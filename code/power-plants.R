f9c0f5e7313b914d068e97b651e8fedae1b409cf
h-hahn <hmhahn19@gmail.com>
2023-08-15 14:45:00 -0700

    fixed document i forgot to save

--- code/power-plants.R
+++ code/power-plants.R
@@ -3,5 +3,11 @@
 library(tidyverse)
 library(here)
 
-plants <- read_csv(here("data", "power_plants.csv"))
+plants <- read_csv(here("data", "power_plants.csv")) %>%
+  clean_names()
 
+alabama_plants <- plants %>% filter(state_name == "Alabama")
+
+alabama_plot <- ggplot(alabama_plants, aes(x = install_mw, y = total_mw)) +
+  geom_point() +
+  theme_light()
